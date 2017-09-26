7ebeacd6b65e9d492ae9759952d3280a6bddf644
Marcin Michalak <plateauu@o2.pl>
2017-09-26 23:02:34 +0200

    Added new view component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,90 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Hero} from './hero';
+
+const HEROES: Hero[] = [
+  {id: 1, name: 'Windstorm'},
+  {id: 2, name: 'Zoe'},
+  {id: 3, name: 'Olo'},
+  {id: 4, name: 'Alison'},
+  {id: 5, name: 'Noah'},
+  {id: 6, name: 'Cindy'},
+  {id: 7, name: 'Brian'}
+];
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+          (click)="onSelect(hero)"
+          [class.selected]="hero === selectedHero">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <hero-detail [hero]="selectedHero"></hero-detail>
+  `,
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607d8b;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
+
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
+
